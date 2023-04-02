10a9ff48972d479b5fa598c752786a1148a60746
Siryi-Oleksandr <siryyoleksandr@gmail.com>
2023-04-02 16:50:54 +0300

    add user email

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -9,7 +9,7 @@ function UserMenu() {
 
   return (
     <div>
-      <p>Welcome, {user.name}</p>
+      <p>Welcome, {user.email}</p>
       <button type="button" onClick={() => dispatch(logOut())}>
         Logout
       </button>
